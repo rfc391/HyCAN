3b3ce06e55ffd4851a1cfbf660a101998f2273aa
Robert Shubert <45332273+rfc391@users.noreply.github.com>
2024-12-16 12:22:50 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,1 @@
-# HyCAN Utilities
-import pandas as pd
-
-def load_data(filepath):
-    return pd.read_csv(filepath)
-
-def summarize_data(data):
-    print(data.describe())
-
-if __name__ == "__main__":
-    print("Run as utility module only.")
+# Hy
