2ea32d21a8ab78be05578eadca1c863aaa2993bf
Robert Shubert <robs@apexsecurityint.com>
2024-12-20 14:31:21 +0000

    Update setup.py
    Signed-off-by: Robert Shubert <robs@apexsecurityint.com>

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
-Placeholder for setup.py
+from setuptools import setup, find_packages
+
+setup(
+	name='HyCAN',
+	version='0.1',
+	packages=find_packages(),
+	install_requires=[
+		# Add your project's dependencies here
+	],
+	entry_points={
+		'console_scripts': [
+			# Add your console scripts here
+		],
